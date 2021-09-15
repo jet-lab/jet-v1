ad6210d239cdf83182c7fb2cad160dc07055265e
Tristyn <tristynstimpson@gmail.com>
2021-09-15 08:26:13 -0700

    Improve transaction logging

--- app/src/scripts/programUtil.ts
+++ app/src/scripts/programUtil.ts
@@ -299,6 +299,7 @@ export const sendTransaction = async (
   }
 
   // Sending phase
+  console.log(`Instructions `, transaction);
   const rawTransaction = transaction.serialize();
   const txid = await provider.connection.sendRawTransaction(
     rawTransaction,
