189628304e90432a6e29efd6b615bf884a3ae5e5
Rheum rhabarbarum <32130807+rhubarbgarden@users.noreply.github.com>
2021-10-07 23:13:52 +0000

    Added privacy policy, styling for links

--- app/src/scripts/jet.ts
+++ app/src/scripts/jet.ts
@@ -212,8 +212,8 @@ export const getWalletAndAnchor = async (provider: WalletProvider): Promise<void
             good: false,
             header: dictionary[preferredLanguage].copilot.alert.warning,
             text: dictionary[preferredLanguage].copilot.alert.disclaimer
-              .replaceAll('{{TERMS OF USE}}', `<a href="https://www.jetprotocol.io/terms-of-use" target="_blank">TERMS OF USE</a>`)
-              .replaceAll('{{PRIVACY POLICY}}', ""),
+              .replaceAll('{{TERMS OF USE}}', `<a href="https://www.jetprotocol.io/terms-of-use" target="_blank" class="bicyclette-bold text-gradient">TERMS OF USE</a>`)
+              .replaceAll('{{PRIVACY POLICY}}', `<a href="https://www.jetprotocol.io/privacy-policy" target="_blank" class="bicyclette-bold text-gradient">PRIVACY POLICY</a>`),
             action: {
               text: dictionary[preferredLanguage].copilot.alert.accept,
               onClick: () => localStorage.setItem('jetDisclaimer', 'true')
